74430f31e495a8e930e6d70fb93cb0241b246c7c
valudollar <valerie.luxuanhui@gmail.com>
2023-08-01 13:36:48 +0800

    frog numba wan

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import frog from "./assets/frog.mp4";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <video
+        width="320"
+        height="320"
+        src={frog}
+        muted
+        autoPlay={"autoplay"}
+        preLoad="auto"
+        loop
+      >
+        <source src={frog} type="video/mp4"></source>
+      </video>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
